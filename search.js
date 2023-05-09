bd130f19a6a94f0b249d718b2cc052e13ade286a
Tanntech <1320203939@bitraipur.ac.in>
2023-05-09 23:06:24 +0530

    Landing Page Elements

---
+++ search.js
@@ -0,0 +1,18 @@
+const searchInput = document.getElementById('search-input');
+const searchButton = document.getElementById('search-button');
+
+searchButton.addEventListener('click', () => {
+  const query = searchInput.value.trim();
+  if (query !== '') {
+    window.location.href = `/search?q=${query}`;
+  }
+});
+
+searchInput.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    const query = searchInput.value.trim();
+    if (query !== '') {
+      window.location.href = `/search?q=${query}`;
+    }
+  }
+});
